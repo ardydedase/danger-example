8058ac9a307ff295b73e47e1abe73893ec8673ee
ardydedase <ardy.dedase@gmail.com>
2022-05-05 16:38:39 -0600

    Ensure PR assignees

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Install
+## Installation
 ```
 npm install
 ```
@@ -8,7 +8,3 @@ Run a dev copy of danger-js. This will not comment on the PR.
 export DANGER_GITHUB_API_TOKEN=<yourtoken>
 node_modules/danger/distribution/commands/danger-pr.js https://github.com/danger/danger-js/pull/1192
 ```
-
-Output will be: 
-
-
--- dangerfile.ts
+++ dangerfile.ts
@@ -1,8 +1,15 @@
-import {danger, warn, markdown} from 'danger';
+import {danger, warn, fail} from 'danger';
 const reviewLargePR = () => {
     const bigPRThreshold = 300;
     if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
         warn(`:exclamation: Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR for faster, easier review.`);
     }
 }
+const ensureAssignees = () => {
+    // Always ensure we assign someone, so that our Slackbot can do its work correctly
+    if (danger.github.pr.assignee === null) {
+    fail("Please assign someone to merge this PR, and optionally include people who should review.")
+  }
+}
 reviewLargePR();
+ensureAssignees();
