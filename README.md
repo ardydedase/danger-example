8b6eafb27269287f374eb3b6e4dc3e2a31b97a25
ardydedase <ardy.dedase@gmail.com>
2022-05-05 16:22:35 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Install
+```
+npm install
+```
+
+Run a dev copy of danger-js. This will not comment on the PR.
+```
+export DANGER_GITHUB_API_TOKEN=<yourtoken>
+node_modules/danger/distribution/commands/danger-pr.js https://github.com/danger/danger-js/pull/1192
+```
+
+Output will be: 
+
+
---
+++ dangerfile.ts
@@ -0,0 +1,8 @@
+import {danger, warn, markdown} from 'danger';
+const reviewLargePR = () => {
+    const bigPRThreshold = 300;
+    if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
+        warn(`:exclamation: Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR for faster, easier review.`);
+    }
+}
+reviewLargePR();
